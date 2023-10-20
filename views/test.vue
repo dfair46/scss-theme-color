24f46a0b2600c193dd2e2b590dcc7e8440141b8b
dfair46 <55684527+dfair46@users.noreply.github.com>
2023-10-20 15:21:00 +0800

    Update test.vue

--- views/test.vue
+++ views/test.vue
@@ -1,6 +1,7 @@
 <template>
   <div class='theme-color'>啦啦啦</div>
   <div class='scss-theme-color'>啦啦啦2</div>
+  <div class='animate-icon'></div>
 </template>
 
 <script setup lang="ts">
@@ -10,9 +11,18 @@
 @import '../scss-color.scss';
 
 .theme-color {
-  color: var(--theme-color)
+  color: var(--theme-color);
+  font-size: $getpx(14);
 }
 .scss-theme-color {
-  color: $theme-color
+  color: $theme-color;
+  @include overflow-ellipsis(2);
+}
+
+.animate-icon{
+  width: 20px;
+  height: 20px;
+  background: green
+  animation: rotate 1.5s linear infinite; /* 添加旋转动画，无限循环 */
 }
 </style>
