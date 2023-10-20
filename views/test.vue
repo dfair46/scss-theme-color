1db2688ac5a34d7917761b885e8f06f409d097a6
dfair46 <55684527+dfair46@users.noreply.github.com>
2023-10-20 15:16:09 +0800

    Create test.vue

---
+++ views/test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class='theme-color'>啦啦啦</div>
+  <div class='scss-theme-color'>啦啦啦2</div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss">
+@import '../../scss-color.scss';
+
+.theme-color {
+  color: var(--theme-color)
+}
+.scss-theme-color {
+  color: $theme-color
+}
+</style>
