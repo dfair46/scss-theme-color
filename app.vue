b1560706e47b9a29d60f3100aa73c1d260266bde
dfair46 <55684527+dfair46@users.noreply.github.com>
2023-10-20 15:08:12 +0800

    Create app.vue

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss">
+@import './scss-color.scss';
+
+:root {
+  --suancai-theme-color: $theme-color;
+}
+</style>
